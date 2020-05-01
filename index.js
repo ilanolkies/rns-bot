3717cc35b9cf41bd6f3b195804fe8054009912ba
Ilan Olkies <ilanolkies@outlook.com>
2020-05-01 18:08:16 -0300

    Add dotenv

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const Web3 = require('web3');
 const RNS = require('@rsksmart/rns');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+const token = process.env.TELEGRAM_BOT_TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
