c9956a70c104be71bdbc5dc6ac012b97a5e52161
Ilan Olkies <ilanolkies@outlook.com>
2020-05-01 18:02:27 -0300

    Reponse the address of the captured domain in the message

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const TelegramBot = require('node-telegram-bot-api');
+const Web3 = require('web3');
+const RNS = require('@rsksmart/rns');
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = 'YOUR_TELEGRAM_BOT_TOKEN';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
+
+// Connect to the RSK network
+const web3 = new Web3('https://public-node.rsk.co');
+
+// Connect to the RNS service
+const rns = new RNS(web3);
 
 // Matches "/addr [whatever]"
 bot.onText(/\/addr (.+)/, (msg, match) => {
@@ -13,8 +21,15 @@ bot.onText(/\/addr (.+)/, (msg, match) => {
   // of the message
 
   const chatId = msg.chat.id;
-  const resp = `I received ${match[1]}`; // the captured [whatever]
 
-  // answer with resp message
-  bot.sendMessage(chatId, resp);
+  // Query the address of the captured [whatever]
+  rns.addr(match[1])
+    .then(addr =>  { // answer the address
+      const resp = `The address is ${addr}`;
+      bot.sendMessage(chatId, resp)
+    })
+    .catch(error => { // answer error when not found and a reference if existent
+      const resp = `Error: ${error.message}. ${error.ref ? `Find more info at ${error.ref}` : ``}`;
+      bot.sendMessage(chatId, resp)
+    });
 });
